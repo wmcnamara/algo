4b14c73d7ba9ec6db32e23dcb055d715ffb35eb7
Weston Mcnamara <westonfmcnamara@gmail.com>
2022-01-22 18:11:50 -0500

    Added problem 6

---
+++ Challenges/ProjectEuler/problem6.cpp
@@ -0,0 +1,22 @@
+//Weston McNamara
+//Problem 6 https://projecteuler.net/problem=6
+
+#include <iostream>
+
+int main()
+{
+	unsigned long long sumOfSquares = 0;
+	unsigned long long squareOfSums = 0;
+
+	for (int i = 1; i <= 100; i++)
+	{
+		sumOfSquares += i * i;
+		squareOfSums += i;
+	}
+
+	squareOfSums = squareOfSums * squareOfSums;
+
+	unsigned long long difference = squareOfSums - sumOfSquares;
+
+	std::cout << difference;
+}
