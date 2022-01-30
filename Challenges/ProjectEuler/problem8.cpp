a6578e07569e0b2ae39a63722f5ca52cb9334875
Weston Mcnamara <westonfmcnamara@gmail.com>
2022-01-30 09:58:24 -0500

    added problem 8 project euler

---
+++ Challenges/ProjectEuler/problem8.cpp
@@ -0,0 +1,24 @@
+//Weston McNamara
+//Problem 8 https://projecteuler.net/problem=8
+
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	for (long long a = 1; a < 1000; a++)
+	{
+		for (long long b = a + 1; b < 1000; b++)
+		{
+			long long aSqr = a * a;
+			long long bSqr = b * b;
+			long long c = round(sqrt(aSqr + bSqr));
+
+			if (a + b + c == 1000 && aSqr + bSqr == c * c)
+			{
+				std::cout << a * b * c << '\n';
+				break;
+			}
+		}
+	}
+}
